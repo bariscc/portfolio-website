b9d3f648a2426a78f47d3585e7a5abab925fa2ee
baris can ceylan <bcanceylan@gmail.com>
2018-08-12 14:33:59 -0700

    update addr

--- php/getmail.php
+++ php/getmail.php
@@ -1,7 +1,7 @@
 <?php
 $value=$_POST['value'];
 if($value == 2) {
-  $arr = ['status'=>'1','value'=>'hi@bariscc.me'];
+  $arr = ['status'=>'1','value'=>'bcanceylan@gmail.com'];
 } else {
   $arr = ['status'=>'0','value'=>'try again'];
 }
